6cae0d4b5d8bdb4c31def70186c4357660a7bc3d
Braian Ledantes <braianledantes@gmail.com>
2025-04-28 11:17:01 -0300

    New Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Footer = ({}) => {
+const Footer = () => {
     return (
-        <footer className="bg-gray-200 p-4">
-            <p className="text-center">Recetas App &copy; 2023</p>
+        <footer className="bg-amber-600 text-white p-8 flex justify-center items-center gap-3">
+            <p>&copy; 2025 Grupo 2, Los Atómicos ⚛️</p>
+            <p>
+                <a href="https://github.com/braianledantes/tp-react-meal" target="_blank" rel="noopener noreferrer" className="underline">
+                    <img src="/github-mark.svg" alt="Logo de Github" className='w-6 h-6' />
+                </a>
+            </p>
+            <p><a href="/Enunciado.pdf" target="_blank" rel="noopener noreferrer" className="underline">Enunciado</a></p>
         </footer>
     );
-  };
-  
+};
+
 export default Footer;
