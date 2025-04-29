95dd1678d7eadbe1d5b841921c54287d6e01cee7
Braian Ledantes <braianledantes@gmail.com>
2025-04-29 12:08:14 -0300

    Refactor: enhance Details component layout and add ingredient measurements

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -30,25 +30,73 @@ export default function Details() {
                 </div>}
             {error && <h1>{error.message}</h1>}
             {meal && (
-                <div>
-                    <iframe width="560" height="315" src={getEmbedYoutubeVideo()} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                    <img src={meal.strMealThumb} alt={meal.strMeal} />
-                    <h2>{meal.strMeal}</h2>
-                    <p>{meal.strInstructions}</p>
-                    <ul>
-                        {meal.strIngredient1 && <li>{meal.strIngredient1}</li>}
-                        {meal.strIngredient2 && <li>{meal.strIngredient2}</li>}
-                        {meal.strIngredient3 && <li>{meal.strIngredient3}</li>}
-                        {meal.strIngredient4 && <li>{meal.strIngredient4}</li>}
-                        {meal.strIngredient5 && <li>{meal.strIngredient5}</li>}
-                    </ul>
-
-                    <button
-                        onClick={handleFavoriteClick}
-                        className={`favorite-btn ${isFavorite(id) ? 'is-favorite' : ''}`}
-                    >
-                        {isFavorite(id) ? 'Quitar de favoritos' : 'Añadir a favoritos'}
-                    </button>
+                <div className='grid grid-cols-[repeat(auto-fit,minmax(360px,1fr))] gap-4 p-4 mx-auto max-w-6xl'>
+                    <div>
+                        <iframe className='w-full aspect-video' src={getEmbedYoutubeVideo()} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                        <h2>{meal.strMeal}</h2>
+                        <button
+                            onClick={handleFavoriteClick}
+                            className={`favorite-btn ${isFavorite(id) ? 'is-favorite' : ''}`}
+                        >
+                            {isFavorite(id) ? 'Quitar de favoritos' : 'Añadir a favoritos'}
+                        </button>
+                    </div>
+                    <div className='flex flex-wrap gap-4'>
+                        <div>
+                            <h3>Instrucciones</h3>
+                            <p>{meal.strInstructions}</p>
+                        </div>
+                        <div>
+                            <h3 className="">Ingredientes</h3>
+                            <ul className="list-disc list-inside">
+                                {meal.strIngredient1 && <li>{meal.strIngredient1}</li>}
+                                {meal.strIngredient2 && <li>{meal.strIngredient2}</li>}
+                                {meal.strIngredient3 && <li>{meal.strIngredient3}</li>}
+                                {meal.strIngredient4 && <li>{meal.strIngredient4}</li>}
+                                {meal.strIngredient5 && <li>{meal.strIngredient5}</li>}
+                                {meal.strIngredient6 && <li>{meal.strIngredient6}</li>}
+                                {meal.strIngredient7 && <li>{meal.strIngredient7}</li>}
+                                {meal.strIngredient8 && <li>{meal.strIngredient8}</li>}
+                                {meal.strIngredient9 && <li>{meal.strIngredient9}</li>}
+                                {meal.strIngredient10 && <li>{meal.strIngredient10}</li>}
+                                {meal.strIngredient11 && <li>{meal.strIngredient11}</li>}
+                                {meal.strIngredient12 && <li>{meal.strIngredient12}</li>}
+                                {meal.strIngredient13 && <li>{meal.strIngredient13}</li>}
+                                {meal.strIngredient14 && <li>{meal.strIngredient14}</li>}
+                                {meal.strIngredient15 && <li>{meal.strIngredient15}</li>}
+                                {meal.strIngredient16 && <li>{meal.strIngredient16}</li>}
+                                {meal.strIngredient17 && <li>{meal.strIngredient17}</li>}
+                                {meal.strIngredient18 && <li>{meal.strIngredient18}</li>}
+                                {meal.strIngredient19 && <li>{meal.strIngredient19}</li>}
+                                {meal.strIngredient20 && <li>{meal.strIngredient20}</li>}
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>Medidas</h3>
+                            <ul className="list-disc list-inside">
+                                {meal.strMeasure1 && <li>{meal.strMeasure1}</li>}
+                                {meal.strMeasure2 && <li>{meal.strMeasure2}</li>}
+                                {meal.strMeasure3 && <li>{meal.strMeasure3}</li>}
+                                {meal.strMeasure4 && <li>{meal.strMeasure4}</li>}
+                                {meal.strMeasure5 && <li>{meal.strMeasure5}</li>}
+                                {meal.strMeasure6 && <li>{meal.strMeasure6}</li>}
+                                {meal.strMeasure7 && <li>{meal.strMeasure7}</li>}
+                                {meal.strMeasure8 && <li>{meal.strMeasure8}</li>}
+                                {meal.strMeasure9 && <li>{meal.strMeasure9}</li>}
+                                {meal.strMeasure10 && <li>{meal.strMeasure10}</li>}
+                                {meal.strMeasure11 && <li>{meal.strMeasure11}</li>}
+                                {meal.strMeasure12 && <li>{meal.strMeasure12}</li>}
+                                {meal.strMeasure13 && <li>{meal.strMeasure13}</li>}
+                                {meal.strMeasure14 && <li>{meal.strMeasure14}</li>}
+                                {meal.strMeasure15 && <li>{meal.strMeasure15}</li>}
+                                {meal.strMeasure16 && <li>{meal.strMeasure16}</li>}
+                                {meal.strMeasure17 && <li>{meal.strMeasure17}</li>}
+                                {meal.strMeasure18 && <li>{meal.strMeasure18}</li>}
+                                {meal.strMeasure19 && <li>{meal.strMeasure19}</li>}
+                                {meal.strMeasure20 && <li>{meal.strMeasure20}</li>}
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             )}
         </>
