625947ab3ff19fc05a5563437b7a57b6f325fb70
Luciana Romano <luciana.romano@est.fi.uncoma.edu.ar>
2025-04-30 17:11:25 -0300

    :art: probando boton

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import PATHS from '../../routes/paths';
-import { Heart } from 'lucide-react';
+import { Heart, Languages} from 'lucide-react';
 
 const Header = () => {
     return (
@@ -14,7 +14,7 @@ const Header = () => {
         <nav >
             <ul className="flex gap-4">
                 <li><Link to={PATHS.FAVORITES}><Heart /></Link></li>
-                <li className="bg-white text-amber-700 px-4 py-2 rounded-full font-medium hover:bg-amber-600 hover:text-white transition-colors duration-300"><Link to={PATHS.HOME}>Idioma</Link></li>
+                <li><Link to={PATHS.HOME}><Languages /></Link></li>
             </ul>
         </nav>
       </header>
