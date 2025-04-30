a5c0f151a9d539a67b8ee1258e149a745db094ac
Braian Ledantes <braianledantes@gmail.com>
2025-04-30 12:18:12 -0300

    💄 componente Search con tailwind

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,13 +1,10 @@
 import { useTranslation } from 'react-i18next';
-import styles from './Search.module.css';
 import { Search as SearchIcon } from 'lucide-react';
 import { useState, useEffect } from 'react';
 
 export default function Search({ searchTerm, onSearch }) {
-
   const { t } = useTranslation();
-  
-  const [inputValue, setInputValue] = useState(searchTerm);  
+  const [inputValue, setInputValue] = useState(searchTerm);
 
   useEffect(() => {
     setInputValue(searchTerm);
@@ -15,18 +12,21 @@ export default function Search({ searchTerm, onSearch }) {
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
-    onSearch(e.target.value);  
+    onSearch(e.target.value);
   };
+
   return (
-    <div className={styles.buscadorContenedor}  >
-      <SearchIcon className={styles.SearchIcon} />
-      <input
-        type="text"
-        placeholder={t('search-placholder')}
-        value={inputValue}
-        onChange={handleChange}
-        className={styles.buscador}
-      />
+    <div className="flex justify-center items-center p-4">
+      <div className="flex justify-center items-center w-full max-w-lg rounded-3xl border border-black p-1 gap-2">
+        <SearchIcon className="text-black ml-2" />
+        <input
+          type="text"
+          placeholder={t('search-placholder')}
+          value={inputValue}
+          onChange={handleChange}
+          className="bg-transparent border-none outline-none px-4 py-2 text-base text-black w-full max-w-lg rounded-full"
+        />
+      </div>
     </div>
   );
 }
