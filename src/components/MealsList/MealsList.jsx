f9396b4143dafc11d7783428ca7369cc1523dbf4
Braian Ledantes <braianledantes@gmail.com>
2025-04-30 13:29:58 -0300

    ♻️ La lista no muestra un mensaje si no hay elementos

--- src/components/MealsList/MealsList.jsx
+++ src/components/MealsList/MealsList.jsx
@@ -1,8 +1,8 @@
 import useFavorites from "../../hooks/useFavorites";
 import MealCard from "../MealCard/MealCard";
 
-export default function MealsList({ meals }) {
-    const { isFavorite, removeFavorite, addFavorite } = useFavorites();
+export default function MealsList({meals}) {
+    const {isFavorite, removeFavorite, addFavorite} = useFavorites();
 
     const handleClickFavorites = (id) => {
         if (isFavorite(id)) {
@@ -14,20 +14,19 @@ export default function MealsList({ meals }) {
 
     return (
         <>
-            {meals.length === 0 && <p className="text-center text-gray-600"> No se encontraron resultados. </p>}
-            {meals.length > 0 &&
-                <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-3 justify-center p-4 max-w-[1000px] mx-auto">
-                    {meals.map((meal) => (
-                        <MealCard
-                            key={meal.idMeal}
-                            id={meal.idMeal}
-                            image={meal.strMealThumb}
-                            title={meal.strMeal}
-                            isFav={isFavorite(meal.idMeal)}
-                            onClickFavorites={handleClickFavorites}
-                        />
-                    ))}
-                </div>
-            }</>
+            <div
+                className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-3 justify-center p-4 max-w-[1000px] mx-auto">
+                {meals.map((meal) => (
+                    <MealCard
+                        key={meal.idMeal}
+                        id={meal.idMeal}
+                        image={meal.strMealThumb}
+                        title={meal.strMeal}
+                        isFav={isFavorite(meal.idMeal)}
+                        onClickFavorites={handleClickFavorites}
+                    />
+                ))}
+            </div>
+        </>
     );
 }
