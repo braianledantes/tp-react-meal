5a793d6387dc6810f16101ca14a7cd78fec32b42
Braian Ledantes <braianledantes@gmail.com>
2025-04-30 13:32:00 -0300

    ♻️ no hay tiempo para describir, me tengo que ir

--- src/hooks/useFavorites.js
+++ src/hooks/useFavorites.js
@@ -1,29 +1,40 @@
-import { useState, useEffect } from 'react';
+import {useEffect, useState} from 'react';
 
 const useFavorites = () => {
     const localStorageKey = 'favoriteMeals';
-    const [favorites, setFavorites] = useState(() => {
-        const storedFavorites = localStorage.getItem(localStorageKey);
-        return storedFavorites ? JSON.parse(storedFavorites) : [];
-    });
+    const [favorites, setFavorites] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        localStorage.setItem(localStorageKey, JSON.stringify(favorites));
-    }, [favorites]);
+        setLoading(true);
+        try {
+            const storedFavorites = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+            setFavorites(storedFavorites);
+        } catch (err) {
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+    }, [])
 
     const addFavorite = (mealId) => {
         if (!favorites.includes(mealId)) {
-            setFavorites((prevFavorites) => [...prevFavorites, mealId]);
+            const updatedFavorites = [...favorites, mealId];
+            setFavorites(updatedFavorites);
+            localStorage.setItem(localStorageKey, JSON.stringify(updatedFavorites));
         }
-    };
+    }
 
     const removeFavorite = (mealId) => {
-        setFavorites((prevFavorites) => prevFavorites.filter((id) => id !== mealId));
-    };
+        const updatedFavorites = favorites.filter((id) => id !== mealId);
+        setFavorites(updatedFavorites);
+        localStorage.setItem(localStorageKey, JSON.stringify(updatedFavorites));
+    }
 
     const isFavorite = (mealId) => favorites.includes(mealId);
 
-    return { favorites, addFavorite, removeFavorite, isFavorite };
+    return {favorites, loading, error, addFavorite, removeFavorite, isFavorite};
 };
 
 export default useFavorites;
--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,70 +1,52 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router';
+import React, {useEffect, useState} from 'react';
 import useFavorites from '../../hooks/useFavorites';
-import { getMealById } from '../../api/mealApi';
+import {getMealById} from '../../api/mealApi';
 import PATHS from '../../routes/paths';
 import Search from "../../components/Search/Search";
+import MealsList from "../../components/MealsList/MealsList.jsx";
+import CircleProgressBar from "../../components/CircleProgressBar/CircleProgressBar.jsx";
+import {useTranslation} from "react-i18next";
 
 
 export default function FavoritesPage() {
+    const {t} = useTranslation();
     const [searchTerm, setSearchTerm] = useState("");
-    const { favorites, removeFavorite } = useFavorites();
+    const {favorites, loading, error} = useFavorites();
     const [meals, setMeals] = useState([]);
 
     const filteredMeals = meals.filter((meal) =>
-      meal.strMeal.toLowerCase().includes(searchTerm.toLowerCase())
+        meal.strMeal.toLowerCase().includes(searchTerm.toLowerCase())
     );
-    
+
     useEffect(() => {
         const fetchMeals = async () => {
-          try {
-            const mealDetails = await Promise.all(
-              favorites.map(async (mealId) => {
-                const meal = await getMealById(mealId);
-                if (meal) {
-                  meal.detailUrl = PATHS.DETAILS.replace(':id', mealId);
-                  return meal;
-                }
-                return null;
-              })
-            );
-            setMeals(mealDetails.filter(meal => meal !== null));
-          } catch (error) {
-            console.error("Error fetching meals:", error);
-          }
+            try {
+                const mealDetails = await Promise.all(
+                    favorites.map(async (mealId) => {
+                        const meal = await getMealById(mealId);
+                        if (meal) {
+                            meal.detailUrl = PATHS.DETAILS.replace(':id', mealId);
+                            return meal;
+                        }
+                        return null;
+                    })
+                );
+                setMeals(mealDetails.filter(meal => meal !== null));
+            } catch (error) {
+                console.error("Error fetching meals:", error);
+            }
         };
-      
+
         fetchMeals();
-      }, [favorites]);
-      
-    if (favorites.length === 0) {
-        return <h2>No tienes favoritos guardados. ¡Agrega algunos!</h2>;
-    }
+    }, [favorites]);
 
     return (
-      <div>
-          <h2>Comidas Favoritas</h2>
-          <Search searchTerm={searchTerm} onSearch={setSearchTerm} />
-            {filteredMeals.length === 0 ? (
-              <p>No se encontró ninguna receta guardada con ese nombre.</p>
-            ) : (
-          <div className="favorites-list">
-              {filteredMeals.map((meal) => (
-                 meal ? ( 
-                  <div key={meal.idMeal} className="favorite-item">
-                      <Link to={meal.detailUrl}>
-                          <img src={meal.strMealThumb} alt={meal.strMeal} className="favorite-item-img" />
-                          <h3>{meal.strMeal}</h3>
-                      </Link>
-                       <button onClick={() => removeFavorite(meal.idMeal)} className="remove-btn">
-                          Eliminar de favoritos
-                      </button>
-                  </div>
-                ) : null
-              ))}
-          </div>
-            )}
-      </div>
+        <>
+            <h2 className="text-4xl text-center mt-4 font-semibold">{t("favorites-title")}</h2>
+            <Search searchTerm={searchTerm} onSearch={setSearchTerm}/>
+            {loading && <div className="flex justify-center items-center mt-16"><CircleProgressBar/></div>}
+            {error && <p>{error.message}</p>}
+            {filteredMeals && <MealsList meals={filteredMeals}/>}
+        </>
     );
 }
